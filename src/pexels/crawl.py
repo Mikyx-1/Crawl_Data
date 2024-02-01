d0843ff43ea65d26e43070835559ee3452359440
Mikyx1 <92131994+Mikyx-1@users.noreply.github.com>
2024-02-01 17:03:03 +0700

    Update crawl.py
    Add position finder

--- src/pexels/crawl.py
+++ src/pexels/crawl.py
@@ -4,11 +4,23 @@ from bs4 import BeautifulSoup
 import time
 import subprocess
 
+def find_vid_urls(string_doc, keyword='https://images.pexels.com/'):
+    start_idx = 0
+    end_idx = len(string_doc) - len(keyword)
+    window_length = len(keyword)
+    results = []
+    for i in range(start_idx, end_idx):
+        if string_doc[i:i+window_length] == keyword:
+            results.append(i)
+    return results
+
 driver = webdriver.Chrome()
 driver.get("https://www.pexels.com/vi-vn/tim-kiem/ng%C6%B0%E1%BB%9Di/") # Put link here
 time.sleep(5)
 soup = BeautifulSoup(driver.page_source, "html.parser")
 string_doc = str(soup)
+
+positions = find_vid_urls(string_doc)
 
 
 results = []
